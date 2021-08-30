ef01bb855063509f817d86958aaa82af8310cc70
EC2 Default User <ec2-user@ip-172-31-48-189.ec2.internal>
2021-08-30 14:43:32 +0000

    add new file

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
   root to: 'homes#top'  
- resources :blogs
- resources :homes
+ resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get 'top' => 'homes#top'
-  get 'books' => 'books#new'
-  #get 'index' => 'books#index'
-  post 'books' => 'books#create'
+ # get 'books' => 'books#new'
+ # get 'index' => 'books#index'
+ # post 'books' => 'books#create'
   
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
+#   get 'books/:id' => 'books#show', as: 'book'
+#   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+#   patch 'books/:id' => 'books#update', as: 'update_book'
   
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+#   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   
 end
  
